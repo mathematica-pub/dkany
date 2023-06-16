b3694dcf4f1fab3ebcf0e6bc0a259a0e395c42c2
dmcarthur <dmcarthur@mathematica-mpr.com>
2023-06-16 10:17:32 -0600

    update package script

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -4,14 +4,27 @@ if [[ -z $(which python | grep virtual) ]]; then
     exit 1
 fi
 
+aws_profile=$(aws configure list-profiles | grep devops)
+if [ -z "$aws_profile" ]
+then
+    echo "The profile ${aws_profile} has not been established.  Using default."
+    aws_profile="default"
+else
+    echo "AWS_PROFILE=${aws_profile}"
+fi
+
 pipenv sync --dev
 
 python setup.py sdist bdist_wheel
 
-aws codeartifact login --tool twine \
+# note: you may have to configure a region as well. add the following to ~/.aws/config
+# [profile devops]
+# region = us-east-1
+
+AWS_PROFILE=$aws_profile aws codeartifact login --tool twine \
     --domain shared-package-domain \
     --domain-owner 922539530544 \
     --repository shared-package-repository
 
-twine upload --repository codeartifact \
+python -m twine upload --repository codeartifact \
     dist/dkany-0.0.12.tar.gz --verbose
