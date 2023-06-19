ce940e0dd05dcebe7b782bc30cb0f2c72202b645
dmcarthur <dmcarthur@mathematica-mpr.com>
2023-06-19 11:29:50 -0600

    don't expect pipenv in publish script

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ -z $(which python | grep virtual) ]]; then
-    echo "Error: not running from virtual env"
-    echo "USAGE: pipenv run bash scripts/publish_package.sh"
-    exit 1
-fi
-
 aws_profile=$(aws configure list-profiles | grep devops)
 if [ -z "$aws_profile" ]
 then
@@ -17,7 +11,7 @@ fi
 
 pipenv sync --dev
 
-python setup.py sdist bdist_wheel
+pipenv run python setup.py sdist bdist_wheel
 
 AWS_PROFILE=$aws_profile aws --region=us-east-1 codeartifact login --tool twine \
     --domain shared-package-domain \
@@ -27,4 +21,4 @@ AWS_PROFILE=$aws_profile aws --region=us-east-1 codeartifact login --tool twine 
 # lists all compiled distributions, parses the version, sorts, and only keeps the last result.
 latest_distribution=$(ls dist/dkany-*.tar.gz | awk -F"-" '{print $NF, $0}' | sort -V | tail -n 1 | awk '{print $2}')
 
-python -m twine upload --repository codeartifact $latest_distribution --verbose
+pipenv run python -m twine upload --repository codeartifact $latest_distribution --verbose
