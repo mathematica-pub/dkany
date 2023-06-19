546872c309acdda3e8bb4fa6780da6b8809e5f9c
Peter Mariani <pmariani@mathematica-mpr.com>
2023-06-19 11:16:15 -0400

    reformatted tests, updated some scripts

--- tests/test_dkany.py
+++ tests/test_dkany.py
@@ -1,35 +1,44 @@
 from dkany.client import DKANClient as DkanyClient
-import unittest
-
-class TestDkanyClient(unittest.TestCase):
-    def setUp(self):
-        self.client = DkanyClient(
+import pytest
+# ----------------------------------
+# Fixtures
+# ----------------------------------
+@pytest.fixture(scope="function")
+def dkan_client():
+    client = DkanyClient(
             base_url = "https://edit.data.medicaid.gov"
-        )
+    )
+    return client
 
-    def test_check_dataset_exists(self):
-        test_dataset_id = "9e407144-9ed9-5cee-937a-17d65b07a9a7"
-        exists = self.client.check_dataset_exists(test_dataset_id)
+@pytest.fixture(scope="function")
+def read_dataset_id():
+    test_dataset_id = "9e407144-9ed9-5cee-937a-17d65b07a9a7"
+    return test_dataset_id
+
+# ----------------------------------
+# Tests
+# ----------------------------------
+def test_check_dataset_exists(dkan_client,read_dataset_id):
+        exists = dkan_client.check_dataset_exists(read_dataset_id)
         print(f"dataset {exists} exists")
         assert exists == True
-        
-    def test_search(self):
+
+def test_search(dkan_client):
         title_success = "Product Data for Newly Reported Drugs in the Medicaid Drug Rebate Program 2023-02-06-to-2023-02-12"
-        search_success = self.client.search(title=title_success)
+        search_success = dkan_client.search(title=title_success)
         assert len(search_success) == 1
         print("Search success:", len(search_success))
         
         title_fail = "Product Data for asdfasdf"
-        search_fail = self.client.search(title=title_fail)
+        search_fail = dkan_client.search(title=title_fail)
         assert len(search_fail) == 0
         print("Search fail:", len(search_fail))
+
+def test_get_dataset_metadata(dkan_client,read_dataset_id):
+        metadata = dkan_client.get_dataset_metadata(read_dataset_id)
+        assert metadata["identifier"] == read_dataset_id
         
-    def test_get_dataset_metadata(self):
-        test_dataset_id = "9e407144-9ed9-5cee-937a-17d65b07a9a7"
-        metadata = self.client.get_dataset_metadata(test_dataset_id)
-        assert metadata["identifier"] == test_dataset_id
-    
-    def test_get_data_by_dataset_identifier(self):
-        test_dataset_id = "9e407144-9ed9-5cee-937a-17d65b07a9a7"
-        results = self.client.get_data_by_dataset_identifier(test_dataset_id)
-        assert len(results) > 0
+            
+def test_get_data_by_dataset_identifier(dkan_client,read_dataset_id):
+    results = dkan_client.get_data_by_dataset_identifier(read_dataset_id)
+    assert len(results) > 0
