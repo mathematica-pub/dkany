52fa2b0bc0133e6295015a2812a35e4c7fde22d7
dmcarthur <dmcarthur@mathematica-mpr.com>
2025-10-07 15:36:09 -0500

    add akamai test

--- tests/test_akamai.py
+++ tests/test_akamai.py
@@ -1,22 +1,13 @@
-# import os
-# from dkany.data_hosts.sftp import SftpClient
+from dkany.data_hosts.sftp import SftpClient
 
 def test_akamai_sftp_client():
-    pass
-    # akamai_sftp_client = SftpClient(
-    #     host = "cmsstorage.upload.akamai.com",
-    #     username = "sshacs",
-    #     private_key_file = os.environ['DKAN_AKAMAI_PRIVATE_KEY'],
-    #     private_key_pass = os.environ['DKAN_AKAMAI_PRIVATE_KEY_PASSWORD'],
-    #     default_remote_path='/399963/questions.medicaid.gov/production/wwwroot/data/scorecard/'
-    # )
+    akamai_sftp_client = SftpClient(
+        host = "cmsstorage.upload.akamai.com",
+        username = "sshacs",
+        private_key_file = "path/to/file",
+        private_key_pass = "pw"
+    )
 
-    # cwd = akamai_sftp_client.connection.getcwd()
-
-    # print(cwd)
-
-    # output = akamai_sftp_client.connection.listdir()
-
-    # print(output)
-    # raise(ValueError("hi"))
+    cwd = akamai_sftp_client.connection.getcwd()
+    assert cwd == "/"
     
